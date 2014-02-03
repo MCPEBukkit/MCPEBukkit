287ff288c860875cd64051f7fc1e5d4f2d3d1f85
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-02-02 23:24:23 -0600

    Contains Items needed in an MCPE server. ;)

---
+++ src/main/java/net/mcpe/server/Items.java
@@ -0,0 +1,130 @@
+package mcpebukkit;
+
+public class items {
+	public static int IRON_SHOVEL = 256;
+	public static int IRON_PICKAXE = 257;
+	public static int IRON_AXE = 258;
+	public static int FLINT_STEEL, FLINT_AND_STEEL = 259;
+	public static int APPLE = 260;
+	public static int BOW = 261;
+	public static int ARROW = 262;
+	public static int COAL = 263;
+	public static int DIAMOND = 264;
+	public static int IRON_INGOT = 265;
+	public static int GOLD_INGOT = 266;
+	public static int IRON_SWORD = 267;
+	public static int WOODEN_SWORD = 268;
+	public static int WOODEN_SHOVEL = 269;
+	public static int WOODEN_PICKAXE = 270;
+	public static int WOODEN_AXE = 271;
+	public static int STONE_SWORD = 272;
+	public static int STONE_SHOVEL = 273;
+	public static int STONE_PICKAXE = 274;
+	public static int STONE_AXE = 275;
+	public static int DIAMOND_SWORD = 276;
+	public static int DIAMOND_SHOVEL = 277;
+	public static int DIAMOND_PICKAXE = 278;
+	public static int DIAMOND_AXE = 279;
+	public static int STICK, STICKS = 280;
+	public static int BOWL = 281;
+	public static int MUSHROOM_STEW = 282;
+	public static int GOLD_SWORD, GOLDEN_SWORD = 283;
+	public static int GOLD_SHOVEL, GOLDEN_SHOVEL = 284;
+	public static int GOLD_PICKAXE, GOLDEN_PICKAXE = 285;
+	public static int GOLD_AXE, GOLDEN_AXE = 286;
+	public static int STRING = 287;
+	public static int FEATHER = 288;
+	public static int GUNPOWDER = 289;
+	public static int WOODEN_HOE = 290;
+	public static int STONE_HOE = 291;
+	public static int IRON_HOE = 292;
+	public static int DIAMOND_HOE = 293;
+	public static int GOLD_HOE, GOLDEN_HOE = 294;
+	public static int SEEDS = 295;
+	public static int WHEAT_SEEDS = 296;
+	public static int WHEAT = 296;
+	public static int BREAD = 297;
+	public static int LEATHER_CAP = 298;
+	public static int LEATHER_TUNIC = 299;
+	public static int LEATHER_PANTS = 300;
+	public static int LEATHER_BOOTS = 301;
+	public static int CHAIN_HELMET = 302;
+	public static int CHAIN_CHESTPLATE = 303;
+	public static int CHAIN_LEGGINGS = 304;
+	public static int CHAIN_BOOTS = 305;
+	public static int IRON_HELMET = 306;
+	public static int IRON_CHESTPLATE = 307;
+	public static int IRON_LEGGINGS = 308;
+	public static int IRON_BOOTS = 309;
+	public static int DIAMOND_HELMET = 310;
+	public static int DIAMOND_CHESTPLATE = 311;
+	public static int DIAMOND_LEGGINGS = 312;
+	public static int DIAMOND_BOOTS = 313;
+	public static int GOLD_HELMET = 314;
+	public static int GOLD_CHESTPLATE = 315;
+	public static int GOLD_LEGGINGS = 316;
+	public static int GOLD_BOOTS = 317;
+	public static int FLINT = 318;
+	public static int RAW_PORKCHOP = 319;
+	public static int COOKED_PORKCHOP = 320;
+	public static int PAINTING = 321;
+	public static int GOLDEN_APPLE = 322; //WHAT?
+	public static int SIGN = 323;
+	public static int WOODEN_DOOR = 324;
+	public static int BUCKET = 325;
+	//326, 327, 238
+	public static int MINECRART = 329;
+	public static int IRON_DOOR = 330;
+	public static int REDSTONE, REDSTONE_DUST = 331;
+	public static int SNOWBALL = 332;
+	//333
+	public static int LEATHER = 334;
+	public static int BRICK = 336;
+	public static int CLAY = 337;
+	public static int SUGARCANE, SUGAR_CANE, SUGAR_CANES = 338;
+	public static int PAPER = 339;
+	//340
+	public static int SLIMEBALL = 341;
+	//342, 343
+	public static int EGG = 344;
+	public static int COMPASS = 345;
+	//346
+	public static int CLOCK = 347;
+	public static int GLOWSTONE_DUST = 348;
+	//public static int RAW_FISH = 349;
+	//public static int COOKED_FISH = 350;
+	public static int DYE = 351;
+	public static int BONE = 352;
+	public static int SUGAR = 353;
+	public static int CAKE = 345;
+	public static int BED = 346;
+	//347, 348, 349, 350, 351, 352, 353, 354, 355, 356,
+	//public static int COOKIE = 357
+	//358
+	public static int SHEARS = 359;
+	public static int MELON = 360;
+	public static int MELON_SLICE = 361;
+	public static int PUMPKIN_SEEDS = 362;
+	public static int MELON_SEEDS = 363;
+	public static int RAW_BEEF = 363;
+	public static int STEAK = 364;
+	public static int COOKED_BEEF = 364;
+	public static int RAW_CHICKEN = 365;
+	public static int COOKED_CHICKEN = 366;
+	//367 - 382 too long :/
+	public static int SPAWN_EGG = 383;
+	//384 - 391
+	public static int CARROT, CARROTS = 391;
+	public static int POTATO, POTATOES = 392;
+	public static int BAKED_POTATO, BAKED_POTATOES = 393;
+	//394, 395, 396, 397, 398, 399
+	public static int PUMPKIN_PIE = 400;
+	//401, 401, 403, 404
+	public static int NETHER_BRICK = 405;
+	public static int QUARTZ, NETHER_QUARTZ = 406;
+	//407 - 455
+	public static int CAMERA = 456;
+	public static int BEETROOT = 457;
+	public static int BEETROOT_SEEDS, BEETROOT_SEED = 458;
+	public static int BEETROOT_SOUP = 459;
+}
