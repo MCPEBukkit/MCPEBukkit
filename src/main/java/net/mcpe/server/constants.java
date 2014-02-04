fc80414935e2ddd55fd8ad0607854cd16e0cbb03
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-02-04 16:34:22 -0600

    Added Constants ;)

--- src/main/java/net/mcpe/server/constants.java
+++ src/main/java/net/mcpe/server/constants.java
@@ -1,1 +1,56 @@
-//soon
+package mcpebukkit;
+
+public class constants {
+	//Imported from Bukkit & PocketMine, MUST GIVE CREDIT ;)
+	public static final int PMF_LEVEL_DEFLATE_LEVEL = 6;
+	public static final int SURVIVAL = 0;
+	public static final int CREATIVE = 1;
+	public static final int ADVENTURE = 2;
+	//view isn't a real gamemode xD
+	
+	public static final int MAX_CHUNK_RATE = 20;
+	public static final int max_chunks_per_player = 4;
+	public static final int PLAYER_MAX_QUEUE = 1024;
+	
+	public static final int PLAYER_SURVIVAL_SLOTS = 36;
+	public static final int PLAYER_CREATIE_SLOTS = 112;
+	
+	public static final int BLOCK_UPDATE_NORMAL = 1;
+	public static final int BLOCK_UPDATE_RANDOM = 2;
+	public static final int BLOCK_UPDATE_SCHEDULED = 3;
+	public static final int BLOCK_UPDATE_WEEK = 4;
+	public static final int BLOCK_UPDATE_TOUCH = 5;
+	
+	//Players and animals
+	public static final int ENTITY_PLAYER = 1;
+	
+	public static final int ENTITY_MOB = 2;
+	public static final int MOB_CHICKEN = 10;
+	public static final int MOB_COW = 11;
+	public static final int MOB_PIG = 12;
+	public static final int MOB_SHEEP = 13;
+	
+	//Mobs
+	public static final int MOB_ZOMBIE = 32;
+	public static final int MOB_CREEPER = 33;
+	public static final int MOB_SKELETON = 34;
+	public static final int MOB_SPIDER = 35;
+	public static final int MOB_PIGMAN = 36;
+	
+	public static final int ENTITY_OBJECT = 3;
+	public static final int OBJECT_PRIMEDTNT = 65;
+	public static final int OBJECT_ARROW = 80;
+	public static final int OBJECT_PAINTING = 83;
+	
+	public static final int ENTITY_ITEM = 4;
+	
+	public static final int ENTITY_FALLING = 5;
+	public static final int FALLING_SAND = 66;
+	
+	//Tiles ;)
+	//public static final String TILE_SIGN = "Sign";
+	//public static final String TILE_CHEST = "Chest";
+	public static final int CHEST_SLOTS = 27;
+	//public static final String TILE_FURNACE = "Furnace";
+	public static final int FURNACE_SLOTS = 3;
+}
