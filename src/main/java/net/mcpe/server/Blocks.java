70061cf668513178df6828796b1a930ec6e16c9a
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-02-03 18:09:53 -0600

    Changed datatypes to final. o/

--- src/main/java/net/mcpe/server/Blocks.java
+++ src/main/java/net/mcpe/server/Blocks.java
@@ -1,134 +1,134 @@
 package mcpebukkit;
 
 public class blocks {
-	public static int AIR = 0;
-	public static int STONE = 1;
-	public static int GRASS = 2;
-	public static int DIRT = 3;
-	public static int COBBLESTONE, COBBLE = 4;
-	public static int PLANK, PLANKS, WOODEN_PLANK, WOODEN_PLANKS = 5;
-	public static int SAPLING, SAPLINGS = 6;
-	public static int BEDROCK = 7;
-	public static int WATER = 8;
-	public static int STILL_WATER = 9;
-	public static int LAVA = 10;
-	public static int STILL_LAVA = 11;
-	public static int SAND = 12;
-	public static int GRAVEL = 13;
-	public static int GOLD_ORE = 14;
-	public static int IRON_ORE = 15;
-	public static int COAL_ORE = 16;
-	public static int WOOD, TRUNK = 17;
-	public static int LEAVES, LEAVE = 18;
-	public static int SPONGE = 19;
-	public static int GLASS = 20;
-	public static int LAPIS_ORE = 21;
-	public static int LAPIS_BLOCK = 22;
+	public static final int AIR = 0;
+	public static final int STONE = 1;
+	public static final int GRASS = 2;
+	public static final int DIRT = 3;
+	public static final int COBBLESTONE, COBBLE = 4;
+	public static final int PLANK, PLANKS, WOODEN_PLANK, WOODEN_PLANKS = 5;
+	public static final int SAPLING, SAPLINGS = 6;
+	public static final int BEDROCK = 7;
+	public static final int WATER = 8;
+	public static final int STILL_WATER = 9;
+	public static final int LAVA = 10;
+	public static final int STILL_LAVA = 11;
+	public static final int SAND = 12;
+	public static final int GRAVEL = 13;
+	public static final int GOLD_ORE = 14;
+	public static final int IRON_ORE = 15;
+	public static final int COAL_ORE = 16;
+	public static final int WOOD, TRUNK = 17;
+	public static final int LEAVES, LEAVE = 18;
+	public static final int SPONGE = 19;
+	public static final int GLASS = 20;
+	public static final int LAPIS_ORE = 21;
+	public static final int LAPIS_BLOCK = 22;
 	//no 23 ;) ehem, yet.
-	public static int SANDSTONE = 24;
+	public static final int SANDSTONE = 24;
 	//WHAT, NO 25? xD
-	public static int BED_BLOCK = 26;
+	public static final int BED_BLOCK = 26;
 	//I don't like you 27 - 29 :p
-	public static int COBWEB = 30;
-	public static int TALL_GRASS = 31;
-	public static int BUSH, DEAD_BUSH = 32;
+	public static final int COBWEB = 30;
+	public static final int TALL_GRASS = 31;
+	public static final int BUSH, DEAD_BUSH = 32;
 	//Nope, 33 - 34!
-	public static int WOOL = 35;
+	public static final int WOOL = 35;
 	//36 soon ;)
-	public static int DANDELION = 37;
-	public static int ROSE, CYAN_FLOWER = 38;
-	public static int BROWN_MUSHROOM = 39;
-	public static int RED_MUSHROOM = 40;
-	public static int GOLD_BLOCK = 41;
-	public static int IRON_BLOCK = 42;
-	public static int DOUBLE_SLAB, DOUBLE_SLABS = 43;
-	public static int SLAB, SLABS = 44;
-	public static int BRICKS, BRICKS_BLOCK = 45;
-	public static int TNT = 46;
-	public static int BOOKSHELF = 47;
-	public static int MOSS_STONE, MOSSY_STONE = 48;
-	public static int OBSIDIAN = 49;
-	public static int TORCH = 50;
-	public static int FIRE = 51;
+	public static final int DANDELION = 37;
+	public static final int ROSE, CYAN_FLOWER = 38;
+	public static final int BROWN_MUSHROOM = 39;
+	public static final int RED_MUSHROOM = 40;
+	public static final int GOLD_BLOCK = 41;
+	public static final int IRON_BLOCK = 42;
+	public static final int DOUBLE_SLAB, DOUBLE_SLABS = 43;
+	public static final int SLAB, SLABS = 44;
+	public static final int BRICKS, BRICKS_BLOCK = 45;
+	public static final int TNT = 46;
+	public static final int BOOKSHELF = 47;
+	public static final int MOSS_STONE, MOSSY_STONE = 48;
+	public static final int OBSIDIAN = 49;
+	public static final int TORCH = 50;
+	public static final int FIRE = 51;
 	//Don't even...
-	public static int WOOD_STAIRS, WOODEN_STAIRS, OAK_WOOD_STAIRS, OAK_WOODEN_STAIRS = 53;
-	public static int CHEST = 54;
+	public static final int WOOD_STAIRS, WOODEN_STAIRS, OAK_WOOD_STAIRS, OAK_WOODEN_STAIRS = 53;
+	public static final int CHEST = 54;
 	// o/
-	public static int DIAMOND_ORE = 56;
-	public static int DIAMOND_BLOCK = 57;
-	public static int CRAFTING_TABLE, WORKBENCH = 58;
-	public static int WHEAT_BLOCK = 59;
-	public static int FARMLAND = 60;
-	public static int FURNACE = 61;
-	public static int BURNING_FURNACE = 62;
-	public static int SIGN_POST = 63;
-	public static int DOOR_BLOCK, WOODEN_DOOR_BLOCK, WOOD_DOOR_BLOCK = 64;
-	public static int LADDER = 65;
+	public static final int DIAMOND_ORE = 56;
+	public static final int DIAMOND_BLOCK = 57;
+	public static final int CRAFTING_TABLE, WORKBENCH = 58;
+	public static final int WHEAT_BLOCK = 59;
+	public static final int FARMLAND = 60;
+	public static final int FURNACE = 61;
+	public static final int BURNING_FURNACE = 62;
+	public static final int SIGN_POST = 63;
+	public static final int DOOR_BLOCK, WOODEN_DOOR_BLOCK, WOOD_DOOR_BLOCK = 64;
+	public static final int LADDER = 65;
 	//66
-	public static int COBBLE_STAIRS, COBBLESTONE_STAIRS = 67;
-	public static int WALL_SIGN = 68; //different from SIGN_POST?
+	public static final int COBBLE_STAIRS, COBBLESTONE_STAIRS = 67;
+	public static final int WALL_SIGN = 68; //different from SIGN_POST?
 	//69, 70
-	public static int IRON_DOOR_BLOCK = 71;
+	public static final int IRON_DOOR_BLOCK = 71;
 	//72
-	public static int REDSTONE_ORE = 73;
-	public static int GLOWING_REDSTONE_ORE, LIT_REDSTONE_ORE = 74;
+	public static final int REDSTONE_ORE = 73;
+	public static final int GLOWING_REDSTONE_ORE, LIT_REDSTONE_ORE = 74;
 	//75, 76, 77
-	public static int SNOW, SNOW_LAYER = 78;
-	public static int ICE = 79;
-	public static int SNOW_BLOCK = 80;
-	public static int CACTUS = 81;
-	public static int CLAY_BLOCK = 80;
-	public static int REEDS, SUGARCANE_BLOCK = 83;
+	public static final int SNOW, SNOW_LAYER = 78;
+	public static final int ICE = 79;
+	public static final int SNOW_BLOCK = 80;
+	public static final int CACTUS = 81;
+	public static final int CLAY_BLOCK = 80;
+	public static final int REEDS, SUGARCANE_BLOCK = 83;
 	//84
-	public static int FENCE = 85;
-	public static int PUMPKIN = 86;
-	public static int NETHERRACK = 87;
-	public static int SOUL_SAND = 88; //what? Soul sand?
-	public static int GLOWSTONE, GLOWSTONE_BLOCK = 89;
+	public static final int FENCE = 85;
+	public static final int PUMPKIN = 86;
+	public static final int NETHERRACK = 87;
+	public static final int SOUL_SAND = 88; //what? Soul sand?
+	public static final int GLOWSTONE, GLOWSTONE_BLOCK = 89;
 	//90
-	public static int LIT_PUMPKIN, JACK_O_LANTERN = 91;
-	public static int CAKE_BLOCK = 92;
+	public static final int LIT_PUMPKIN, JACK_O_LANTERN = 91;
+	public static final int CAKE_BLOCK = 92;
 	//93, 94, 95
-	public static int TRAPDOOR = 96;
-	public static int STONE_BRICKS, STONE_BRICK = 98;
+	public static final int TRAPDOOR = 96;
+	public static final int STONE_BRICKS, STONE_BRICK = 98;
 	//99, 100
-	public static int IRON_BAR, IRON_BARS = 101;
-	public static int GLASS_PANE, GLASS_PANEL = 102;
-	public static int MELON_BLOCK = 103;
-	public static int PUMPKIN_STEM = 104;
-	public static int MELON_STEM = 105;
+	public static final int IRON_BAR, IRON_BARS = 101;
+	public static final int GLASS_PANE, GLASS_PANEL = 102;
+	public static final int MELON_BLOCK = 103;
+	public static final int PUMPKIN_STEM = 104;
+	public static final int MELON_STEM = 105;
 	//106
-	public static int FENCE_GATE = 107;
-	public static int BRICK_STAIRS = 108;
-	public static int STONE_BRICK_STAIRS = 109;
+	public static final int FENCE_GATE = 107;
+	public static final int BRICK_STAIRS = 108;
+	public static final int STONE_BRICK_STAIRS = 109;
 	//110, 111
-	public static int NETHER_BRICKS, NETHER_BRICK_BLOCK = 112;
+	public static final int NETHER_BRICKS, NETHER_BRICK_BLOCK = 112;
 	//113
-	public static int NETHER_BRICKS_STAIRS = 114;
+	public static final int NETHER_BRICKS_STAIRS = 114;
 	//115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
-	public static int SANDSTONE_STAIRS = 128;
+	public static final int SANDSTONE_STAIRS = 128;
 	//129, 130, 131, 132, 133
-	public static int SPRUCE_WOOD_STAIRS, SPRUCE_WOODEN_STAIRS = 134;
-	public static int BIRCH_WOOD_STAIRS, BIRCH_WOODEN_STAIRS = 135;
-	public static int JUNGLE_WOOD_STAIRS, JUNGLE_WOODEN_STAIRS = 136;
+	public static final int SPRUCE_WOOD_STAIRS, SPRUCE_WOODEN_STAIRS = 134;
+	public static final int BIRCH_WOOD_STAIRS, BIRCH_WOODEN_STAIRS = 135;
+	public static final int JUNGLE_WOOD_STAIRS, JUNGLE_WOODEN_STAIRS = 136;
 	//137, 139
-	public static int COBBLE_WALL, STONE_WALL, COBBLESTONE_WALL = 139;
+	public static final int COBBLE_WALL, STONE_WALL, COBBLESTONE_WALL = 139;
 	//140
-	public static int CARROT_BLOCK = 141;
-	public static int POTATO_BLOCK = 142;
+	public static final int CARROT_BLOCK = 141;
+	public static final int POTATO_BLOCK = 142;
 	//143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154
-	public static int QUARTZ_BLOCK = 155;
-	public static int QUARTZ_STAIRS = 156;
-	public static int DOUBLE_WOOD_SLAB, DOUBLE_WOODEN_SLAB, DOUBLE_WOOD_SLABS, DOUBLE_WOODEN_SLABS = 157;
-	public static int WOOD_SLAB, WOODEN_SLAB, WOOD_SLABS, WOODEN_SLABS = 158;
+	public static final int QUARTZ_BLOCK = 155;
+	public static final int QUARTZ_STAIRS = 156;
+	public static final int DOUBLE_WOOD_SLAB, DOUBLE_WOODEN_SLAB, DOUBLE_WOOD_SLABS, DOUBLE_WOODEN_SLABS = 157;
+	public static final int WOOD_SLAB, WOODEN_SLAB, WOOD_SLABS, WOODEN_SLABS = 158;
 	//159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169
-	public static int HAY_BALE = 170;
-	public static int CARPET = 171;
+	public static final int HAY_BALE = 170;
+	public static final int CARPET = 171;
 	//172
-	public static int COAL_BLOCK = 173;
+	public static final int COAL_BLOCK = 173;
 	//174 - 243, Too long :/
-	public static int BEETROOT_BLOCK = 244;
-	public static int STONECUTTER = 245;
-	public static int GLOWING_OBSIDIAN = 246;
-	public static int NETHER_REACTOR = 247;
+	public static final int BEETROOT_BLOCK = 244;
+	public static final int STONECUTTER = 245;
+	public static final int GLOWING_OBSIDIAN = 246;
+	public static final int NETHER_REACTOR = 247;
 }
