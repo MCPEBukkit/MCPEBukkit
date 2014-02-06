a9a64da345cbef195338e7c893973557c3ca9aff
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-02-05 20:41:44 -0600

    Updated Linux run file o/

--- samplerun.sh
+++ samplerun.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-/*
-Reads dirname, then runs the .jar file (No compile needed to you minejava people xD)
-*/
+#Reads dirname, then runs the .jar file (No compile needed to you minejava people xD)
+
  BINDIR=$(dirname "$(readlink -fn "$0")")
  cd "$BINDIR"
  java -Xmx1024M -jar MCPEBukkit.jar -o true
